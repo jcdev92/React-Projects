9bcfd8b8e586b0cb1c5a2712280aae305a535fbf
Jesus Clemente <jcdeveloper92@gmail.com>
2023-10-25 23:16:29 -0400

    namspace vadilation intro

--- TypeScript/FH-TS/bases/main.js
+++ TypeScript/FH-TS/bases/main.js
@@ -1,45 +1,13 @@
 "use strict";
-(() => {
-    const conducirBatimovil = (auto) => {
-        auto.encender = true;
-        auto.velocidadMaxima = 100;
-        auto.acelear();
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    const batimovil = {
-        encender: false,
-        velocidadMaxima: 0,
-        acelear() {
-            console.log("...... gogogo!!!");
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-    conducirBatimovil(batimovil);
-    const guason = {
-        reir: true,
-        comer: true,
-        llorar: false
-    };
-    const reir = (guason) => {
-        if (guason.reir) {
-            console.log("Joker Says: JAJAJAJA");
-        }
-    };
-    reir(guason);
-    const ciudadGotica = (ciudadanos) => {
-        return ciudadanos.length;
-    };
-    console.log(ciudadGotica(["Joker", "Batman", "Superman", "Robin", "Batwoman"]));
-    class Persona {
-        constructor(nombre, edad, sexo, estadoCivil) {
-            this.nombre = nombre;
-            this.edad = edad;
-            this.sexo = sexo;
-            this.estadoCivil = estadoCivil;
-        }
-        imprimirBio() {
-            console.log(`Nombre: ${this.nombre} \n Edad: ${this.edad} \n Sexo: ${this.sexo} \n Estado Civil: ${this.estadoCivil}`);
-        }
-    }
-    const persona = new Persona("Barbara Gordon", 30, "Femenino", "Soltero");
-    persona.imprimirBio();
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Hola'));
+console.log(Validations.validateDate(new Date()));
 //# sourceMappingURL=main.js.map
