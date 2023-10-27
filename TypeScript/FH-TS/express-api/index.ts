6deed8eb5794114c31ccc9184a5958d7e1ab5fde
Jesus Clemente <jcdeveloper92@gmail.com>
2023-10-27 17:53:51 -0400

    levantando un servidor con express con typescript compilando

---
+++ TypeScript/FH-TS/express-api/index.ts
@@ -0,0 +1,17 @@
+// ecmascript convention not commonJS
+import express from 'express'
+// const express = require('express')
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(401).json({
+        ok: false,
+        msg: "No hay token en la peticion"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
