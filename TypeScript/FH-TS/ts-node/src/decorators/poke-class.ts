a0357b529ad8451f09c509447cfd8c87b26591f6
Jesus Clemente <jcdeveloper92@gmail.com>
2023-10-27 12:36:41 -0400

    factory decorator CheckValidPokemonID

--- TypeScript/FH-TS/ts-node/src/decorators/poke-class.ts
+++ TypeScript/FH-TS/ts-node/src/decorators/poke-class.ts
@@ -3,24 +3,51 @@
 // Los decoradores se ejecutan antes de la creacion de la clase.
 
 function printToConsole(constructor: Function): void {
-    console.log(constructor)
+    console.log(constructor);
 }
 
 
 //* factory decorators
+// factory decorators son funciones que devuelven otras funciones
 const printToConsoleConditional = (print: boolean = false):Function => {
     if (print) {
         return printToConsole;
     } else {
-        return ():void => console.log("greetings")
+        return ():void => console.log("greetings");
     }
 }
 
+function CheckValidPokemonId() {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+        descriptor.value = (id: number) => {
+            if (id < 1 || id > 800) {
+                return console.error('El id del pokemon debe estar entre 1 y 800');
+            } else {
+                return originalMethod(id);
+            }
+        }
+    }
+}
+
+
+const blockPrototype = function(constructor: Function) {
+    Object.seal(constructor);
+    Object.seal(constructor.prototype);
+}
+
+// decoradores anidados
+@blockPrototype
 @printToConsoleConditional(true)
 export class Pokeclass {
     public pubApi: string = `https://pokeapi.co`
     constructor(
         public name: string,
     ) {}
+
+    @CheckValidPokemonId()
+    savePokemonToDB(id: number) {
+        console.log(`Pokemon ${id} guardado en la base de datos `);
+    }
 }
 
--- TypeScript/FH-TS/ts-node/src/index.ts
+++ TypeScript/FH-TS/ts-node/src/index.ts
@@ -1,3 +1,11 @@
 import { Pokeclass } from "./decorators/poke-class";
 
 const charmander = new Pokeclass('Charmander');
+
+
+// intentando expandir el prototipo de la clase
+// (Pokeclass.prototype as any).customName = 'Pikacha' //? no deberia dejarnos ya que con el decorador de bloqueo estamos evitando que esto se pueda hacer
+
+// console.log(charmander); // efectivamente: //!TypeError: Cannot add property customName, object is not extensible
+
+charmander.savePokemonToDB(125)
