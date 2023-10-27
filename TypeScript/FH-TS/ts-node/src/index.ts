e6844c8c391189e4114151d51282fdfd79a210e6
Jesus Clemente <jcdeveloper92@gmail.com>
2023-10-26 20:44:07 -0400

    generics functions type

--- TypeScript/FH-TS/ts-node/src/index.ts
+++ TypeScript/FH-TS/ts-node/src/index.ts
@@ -1,40 +1,9 @@
-//* Import y Export en TypeScript\
+import { printObject } from "./generics/generics";
 
-
-// import { Hero } from "./classes/Hero"; 
-
-// const ironman = new Hero('Ironman', 3, 45);
-// console.log(ironman);
-
-
-//* Alias a una importacion:
-
-//! ya tenemos una vairble hero
-const hero = 134
-
-//? entonces nombramos el objeto importado Hero con un alias
-// import { Hero as SuperHero } from "./classes/Hero";
-// const ironman = new SuperHero('Ironman', 3, 45);
-
-// or 
-
-import * as HeroClasses from "./classes/Hero";
-const ironman = new HeroClasses.Hero('Ironman', 3, 45);
-const ironPower = new HeroClasses.Power(5, 'Flying');
-
-console.log(`hero name: ${ironman.name} and power: ${ironPower.name}`);
-
-// defaul import
-import powers from "./data/powers";
-
-console.log(powers)
-
-
-// home work
-console.log(ironman.power)
-
-
-
-
-
-
+console.log(printObject(123));
+console.log(printObject(new Date()).getDate())
+console.log(printObject({a:1, b:2, c:3}).a)
+console.log(printObject([1,2,3,4,5,6,7,8,9,10]).reduce(
+    (a,b) => a + b
+))
+console.log(printObject('Hola Mundo').toUpperCase())
